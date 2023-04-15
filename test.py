cdff32c2f72fd439120931047a2801a31b1e07bc
Mezz21 <mezzi87@yahoo.com>
2023-04-15 18:08:46 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,26 @@ class Shopper:
     pass
 
 class Product:
-    pass
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def get_name(self):
+        return self.name
+
+    def get_price(self):
+        return self.price
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_price(self, new_price):
+        self.price = new_price
+
+    def set_quantity(self, new_quantity):
+        self.quantity = new_quantity
+
 
 class Coupon:
     pass
