3d1b792708eefc977d5a50bfba46c4633c9a782c
Mezz21 <mezzi87@yahoo.com>
2023-04-15 22:48:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 import random
 
 class GroceryStore:
+    """
+    A class that represents a grocery store.
+
+    Attributes:
+    - name (str): the name of the grocery store
+    - inventory (dict): a dictionary of product names and their quantities in the store
+
+    Methods:
+    - __init__(self, name, inventory): Initializes a grocery store with the name of it and its inventory.
+    - setup_store(self, stock): Populates the inventory of the grocery store with products and their quantities 
+                                from a file named "stock". 
+    """
     def __init__(self, name, inventory):
         # Intitialize a grocery store with the name of it and its inventory.
         self.name = name
@@ -14,6 +26,22 @@ class GroceryStore:
                     self.inventory[item] = quantity
 
 class Shopper: 
+    """
+    A class that represents a shopper.
+
+    Attributes:
+    - cart (list): a list of items in the shopper's cart
+    - budget (float): the amount of money the shopper has to spend
+    - store_prices (dict): a dictionary of product names and their prices in the grocery store
+
+    Methods:
+    - __init__(self, budget, store_prices): Initializes a shopper with a budget and the prices of products 
+                                             in the grocery store.
+    - add(self, item, price, inventory): Adds an item to the shopper's cart if it is available in the inventory 
+                                         and the shopper's budget is sufficient to buy it.
+    - checkout(self): Calculates the total cost of items in the shopper's cart and returns it. 
+                      The cart is then cleared.
+    """
     def init(self, budget, store_prices):
         self.cart = []
         self.budget = budget
@@ -35,7 +63,22 @@ class Shopper:
         return total
 
 class Product:
-    # A class that gives values to the products in the grocery store with names, prices, and quantity
+    """
+    A class that represents a product in a grocery store.
+
+    Attributes:
+    - name (str): the name of the product
+    - price (float): the price of the product
+    - quantity (int): the quantity of the product in the store
+
+    Methods:
+    - __init__(self, name, price, quantity): Initializes a product with its name, price, and quantity.
+    - get_name(self): Returns the name of the product.
+    - get_price(self): Returns the price of the product.
+    - get_quantity(self): Returns the quantity of the product in the store.
+    - set_price(self, new_price): Sets a new price for the product.
+    - set_quantity(self, new_quantity): Sets a new quantity for the product in the store.
+    """
     def __init__(self, name, price, quantity):
         self.name = name
         self.price = price
@@ -58,6 +101,22 @@ class Product:
 
 
 class Coupon:
+    """
+    A class that represents a coupon for a shopper.
+
+    Attributes:
+    - coupons (dict): a dictionary of product names and their discounts for a shopper
+
+    Methods:
+    - __init__(self): Initializes a dictionary of coupons for each shopper.
+    - generate_coupon(self): Generates a random discount (integer between 5 and 20) 
+                             for a random product and adds it to the shopper's coupons dictionary.
+    - get_coupons(self): Returns the shopper's coupons dictionary.
+    - get_discount(self, product_name): Returns the discount for a specific product in the shopper's coupons.
+    - set_discount(self, product_name, new_discount): Sets a new discount for a specific product in the shopper's coupons.
+    - get_product(self, product_name): Loops through Products and checks if the Shopper has a discount for that product.
+    - set_product(self, product_name, new_product): Switches the product on a coupon to a different product.
+    """
 # class that createa a dictionary for each new shopper, 
 # and adds a randomly generated coupon from the products class
     def __init__(self):
