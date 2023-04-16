302eb830e1d9d0c9d518b9350008bcbe49ad461e
Colleen Wang <colleenwang2@gmail.com>
2023-04-15 20:39:01 -0400

    get_coupons, get_discount, get_product, set_discount

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ class Product:
 class Coupon:
 # class that createa a dictionary for each new shopper, 
 # and adds a randomly generated coupon from the products class
-    def __init__(self, shopper):
+    def __init__(self):
     # Creates a dictionary of coupons for each shopper
         self.coupons = {}
     
@@ -42,4 +42,27 @@ class Coupon:
         product = random.choice(Product.products)
         self.coupons[product.get_name()] = discount
         print(f"{Shopper} has a coupon for {discount}% off {product}")
+    
+    def get_coupons(self):
+    # method that returns the shoppers dictionary of coupons
+        return self.coupons
+    
+    def get_discount(self, product_name):
+    # method that gets the discount of a specific product
+        return self.coupons.get(product_name)
+        # might add error message "no coupon" if product not found
+    
+    def set_discount(self, product_name, new_discount):
+    # method that sets new discount for a specific product if needed
+    # or allows for manually created coupon
+        self.coupons[product_name] = new_discount
+        
+    def get_product(self, product_name):
+    # method that loops through Products and checks if the Shopper has
+    # a discount for that product
+        for product in Product.products:
+            if product.get_name() == product_name:
+                return product
+        return None
+    
     
